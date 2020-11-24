738e6e9a4fde71815a5ba067d4890027bc2f588b
sngze <41564885+sngze@users.noreply.github.com>
2020-11-25 01:53:44 +0900

    test

--- Web/model.py
+++ Web/model.py
@@ -24,8 +24,8 @@ class Database():
 
     def select(self):
          date = "{}-{}-{}".format(datetime.today().year, datetime.today().month, datetime.today().day)
-         sql = "select menu, liters from flow_meter where date = %s order by line;"
-         row = self.execute(sql, date)
+         sql = "select menu, liters from flow_meter where date = %s order by line;" % (date)
+         row = self.executeall(sql)
          return row
 
 
