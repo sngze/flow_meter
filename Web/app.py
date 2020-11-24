8c7caa39d73581ea0f0725570326584069c30ca6
tvstar <tvstar1448@gmail.com>
2020-11-25 01:15:04 +0900

    test

---
+++ Web/app.py
@@ -0,0 +1,29 @@
+import json
+import pymysql
+from flask import Flask, request, render_template,jsonify
+from datetime import datetime
+
+global db, cur
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False  
+
+db = pymysql.connect(host="localhost", user="raspi", password="0000", db="brewants", charset="utf8")
+cur = db.cursor()
+
+@app.route('/request2', methods =['POST'])  
+def request2():
+    date = "{}-{}-{}".format(datetime.today().year, datetime.today().month, datetime.today().day)
+    sql = "select menu, liters from flow_meter where date = %s order by line;"
+    cur.execute(sql, date)
+    row = cur.fetchall()
+    print(row)
+    return jsonify(row) 
+
+@app.route('/chart') 
+def form():
+    return render_template('chart.html')
+
+
+if __name__ == '__main__':
+      app.run(host='127.0.0.1', port=5000, debug=True)
